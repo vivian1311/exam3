137d9a9a9971dfd65016e0d9975f82cb8cef83b2
vivian1311 <elvis212332@gmail.com>
2020-06-03 17:02:15 +0800

    add new

--- ex3/mqtt_client.py
+++ ex3/mqtt_client.py
@@ -10,19 +10,66 @@ mqttc = paho.Client()
 host = "localhost"
 topic = "Mbed"
 
+# XBee 
+serdev = '/dev/ttyUSB0'
+s = serial.Serial(serdev, 9600)
+
+s.write("+++".encode())
+char = s.read(2)
+print("Enter AT mode.")
+print(char.decode())
+
+s.write("ATMY 0x140\r\n".encode())
+char = s.read(3)
+print("Set MY 0x140.")
+print(char.decode())
+
+s.write("ATDL 0x240\r\n".encode())
+char = s.read(3)
+print("Set DL 0x240.")
+print(char.decode())
+
+s.write("ATID 0x1\r\n".encode())
+char = s.read(3)
+print("Set PAN ID 0x1.")
+print(char.decode())
+
+s.write("ATWR\r\n".encode())
+char = s.read(3)
+print("Write config.")
+print(char.decode())
+
+s.write("ATMY\r\n".encode())
+char = s.read(4)
+print("MY :")
+print(char.decode())
+
+s.write("ATDL\r\n".encode())
+char = s.read(4)
+print("DL : ")
+print(char.decode())
+
+s.write("ATCN\r\n".encode())
+char = s.read(3)
+print("Exit AT mode.")
+print(char.decode())
+
+print("start sending RPC")
+
 # signal
-v = np.arange(0,2,1/100)
+#v = np.arange(0,100,1/100)
 t = np.arange(0,10,0.1)
-num = 0
+v = np.arange(0,100,0.1)
 # Callbacks
 def on_connect(self, mosq, obj, rc):
       print("Connected rc: " + str(rc))
 def on_message(mosq, obj, msg):
       print("[Received] Topic: " + msg.topic + ", Message: " + str(msg.payload) + "\n")
       input_data = msg.payload.decode(encoding = 'UTF-8')
-      data = input_data
-      for num in range(1, 10):
-          print("%f", input_data)
+      
+      for num in range(0, 100):
+            v[num] = input_data
+            print("%f", v[num])
             
 def on_subscribe(mosq, obj, mid, granted_qos):
       print("Subscribed OK")
